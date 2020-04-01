53bd7c19293e5d5110d081cde4fdd9a8790276ba
Wim Hambrouck <wim.hambrouck@ehb.be>
2020-04-01 13:55:47 +0200

    Model

--- Werkcollege03.Oef03/Models/Vak.cs
+++ Werkcollege03.Oef03/Models/Vak.cs
@@ -4,5 +4,22 @@
     {
         public int ID { get; set; }
         public string Naam { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var vak = (Vak)obj;
+
+            return vak.ID == ID;
+        }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode();
+        }
     }
 }
