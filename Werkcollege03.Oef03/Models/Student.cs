0fdccd596e33cb6a3e356c8c3c8d73b9a03c210c
Wim Hambrouck <wim.hambrouck@ehb.be>
2020-04-01 13:29:22 +0200

    Add project files.

---
+++ Werkcollege03.Oef03/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Werkcollege03.Oef03.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public int Naam { get; set; }
+        public virtual IDictionary<Vak, int> Punten { get; set; }
+
+    }
+}
