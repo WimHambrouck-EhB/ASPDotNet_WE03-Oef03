cdc6bf8ca97bb3d0c6c7cf96939fa358313c4018
Wim Hambrouck <wim.hambrouck@ehb.be>
2020-04-02 17:22:51 +0200

    db seeder

---
+++ Werkcollege03.Oef03/Data/SeedData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Werkcollege03.Oef03.Models;
+
+namespace Werkcollege03.Oef03.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(Werkcollege03Oef03Context context)
+        {
+            context.Database.EnsureCreated();
+
+            if (!context.Vakken.Any())
+            {
+                context.Vakken.AddRange(new Vak[]
+                {
+                    new Vak { Naam = ".NET Advanced" },
+                    new Vak { Naam = "Data Advanced" },
+
+                });
+            }
+
+            if (!context.Studenten.Any())
+            {
+                context.Studenten.AddRange(new Student[] {
+                    new Student { Naam = "Paul" },
+                    new Student { Naam = "Marvin" },
+                    new Student { Naam = "Michael" },
+                    new Student { Naam = "Amber" },
+                    new Student { Naam = "Anna" },
+                    new Student { Naam = "Belle" },
+                    new Student { Naam = "Carrie" },
+                    new Student { Naam = "Wim",
+                        Punten = new List<Punt> {
+                            new Punt {
+                                Score = 20,
+                                Vak = new Vak { Naam = ".NET Essentials" }
+                            }
+                        }
+                    }
+                });
+            }
+
+
+            context.SaveChanges();
+        }
+    }
+}
--- Werkcollege03.Oef03/Models/Punt.cs
+++ Werkcollege03.Oef03/Models/Punt.cs
@@ -1,9 +1,12 @@
-﻿namespace Werkcollege03.Oef03.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Werkcollege03.Oef03.Models
 {
     public class Punt
     {
         public int ID { get; set; }
         public Vak Vak { get; set; }
+        [Range(0, 20)]
         public int Score { get; set; }
 
     }
