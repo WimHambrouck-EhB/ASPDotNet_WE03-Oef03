1ce71f36093bf8fd12eba59f2b0056c615af1652
Wim Hambrouck <wim.hambrouck@ehb.be>
2021-03-26 10:27:28 +0100

    nieuw project

--- Werkcollege03.Oef03/Controllers/HomeController.cs
+++ Werkcollege03.Oef03/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Werkcollege03.Oef03.Models;
 
 namespace Werkcollege03.Oef03.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
         {
             return View();
         }
--- Werkcollege03.Oef03/Models/Vak.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Werkcollege03.Oef03.Models
-{
-    public class Vak
-    {
-        public int ID { get; set; }
-        public string Naam { get; set; }
-    }
-}
